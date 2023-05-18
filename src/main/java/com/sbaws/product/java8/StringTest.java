0d554374b385315d6d9a47a316eba6fc7606c9eb
SATYA <satyamcajava@gmail.com>
2023-05-18 11:15:33 +0530

    third commit

--- src/main/java/com/sbaws/product/java8/StringTest.java
+++ src/main/java/com/sbaws/product/java8/StringTest.java
@@ -1,6 +1,7 @@
 package com.sbaws.product.java8;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -12,5 +13,16 @@ public class StringTest {
         Map<String, Long> countMap = Arrays.stream(str.split(""))
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         System.out.println("Each Char occurance::" + countMap);
+
+        //find all duplicate elements from a given String
+        List<String> duplicateKeys = Arrays.stream(str.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(i -> i.getValue() > 1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+        System.out.println("Duplicate elements above String is::" + duplicateKeys);
+
     }
 }
